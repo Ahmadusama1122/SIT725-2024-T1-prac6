7feef72a9a7a73383f1d1461b4597406156fd054
Usama <admin@Admins-MacBook-Pro.local>
2024-04-12 10:35:43 +1000

    initial code commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,48 +1,127 @@
+// // const clickMe = () => {
+// //     alert("Thanks for clicking me. Hope you have a nice day!")
+// // }
+// // $(document).ready(function () {
+// //     $('.materialboxed').materialbox();
+// //     $('#clickMeButton').click(() => {
+// //         clickMe();
+// //     })
+
+// const { response } = require("express")
+
+// // });
+// const cardList = [
+//     {
+//         title: "Deakin Hall",
+//         image: "images/deakin2.jpeg",
+//         link: "About Hall",
+//         desciption: "This is Hall"
+//     },
+//     {
+//         title: "Library",
+//         image: "images/deakin3.jpeg",
+//         link: "About Library",
+//         desciption: "This is Library"
+//     }
+// ]
 // const clickMe = () => {
 //     alert("Thanks for clicking me. Hope you have a nice day!")
 // }
+// const getallcards = ()=>{
+//     $.get('/api/cards', (response)=>{
+//         if(response.statusCode==200){
+//             addCards(response.formData)
+//         }
+//     })
+// }
+// const submitForm = () => {
+//     let formData = {};
+//     formData.first_name = $('#first_name').val();
+//     formData.last_name = $('#last_name').val();
+//     formData.password = $('#password').val();
+//     formData.email = $('#email').val();
+//     console.log("Form Data Submitted: ", formData);
+// }
+// const addCards = (items) => {
+//     items.forEach(item => {
+//         let itemToAppend = '<div class="col s4 center-align">' +
+//             '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.image + '">' +
+//             '</div><div class="card-content">' +
+//             '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.link + '</a></p></div>' +
+//             '<div class="card-reveal">' +
+//             '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
+//             '<p class="card-text">' + item.desciption + '</p>' +
+//             '</div></div></div>';
+//         $("#card-section").append(itemToAppend)
+//     });
+// }
 // $(document).ready(function () {
 //     $('.materialboxed').materialbox();
-//     $('#clickMeButton').click(() => {
-//         clickMe();
+//     $('#formSubmit').click(() => {
+//         submitForm();
 //     })
+//     addCards(cardList);
+//     $('.modal').modal();
 // });
-const cardList = [
-    {
-        title: "Deakin Hall",
-        image: "images/deakin2.jpeg",
-        link: "About Hall",
-        desciption: "This is Hall"
-    },
-    {
-        title: "Library",
-        image: "images/deakin3.jpeg",
-        link: "About Library",
-        desciption: "This is Library"
-    }
-]
-const clickMe = () => {
-    alert("Thanks for clicking me. Hope you have a nice day!")
-}
-const submitForm = () => {
-    let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
-    console.log("Form Data Submitted: ", formData);
-}
+
+
 const addCards = (items) => {
     items.forEach(item => {
         let itemToAppend = '<div class="col s4 center-align">' +
-            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.image + '">' +
+            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.path + '">' +
             '</div><div class="card-content">' +
-            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.link + '</a></p></div>' +
+            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.subTitle + '</a></p></div>' +
             '<div class="card-reveal">' +
-            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
-            '<p class="card-text">' + item.desciption + '</p>' +
+            '<span class="card-title grey-text text-darken-4">' + "Hello from " + item.title + '<i class="material-icons right">close</i></span>' +
+            '<p class="grey-text text-darken-4">' + item.description + '</p>' +
             '</div></div></div>';
         $("#card-section").append(itemToAppend)
+    });
+}
+
+const submitForm = () => {
+    let formData = {
+        title: $('#title').val(),
+        subTitle: $('#subTitle').val(),
+        path: $('#path').val(),
+        description: $('#description').val()
+    };
+
+    console.log(formData);
+
+    $.ajax({
+        url: '/api/card',
+        type: 'POST',
+        data: formData,
+        success: (result) => {
+            if (result.statusCode === 201) {
+                alert('card post successful');
+                getAllCards();
+            }
+        }
+    });
+};
+
+function postCard(cat){
+    $.ajax({
+        url:'/api/card',
+        type:'POST',
+        data:cat,
+        success: (result)=>{
+            if (result.statusCode === 201) {
+                alert('card added');
+            }
+        }
+    });
+}
+function getAllCards(){
+    $.get('/api/cards', (result) => {
+        if (result.statusCode === 200) {
+            addCards(result.data);
+        }
+    })
+    .fail((jqXHR, textStatus, errorThrown) => {
+        console.error(jqXHR.responseText);
     });
 }
 $(document).ready(function () {
@@ -50,7 +129,8 @@ $(document).ready(function () {
     $('#formSubmit').click(() => {
         submitForm();
     })
-    addCards(cardList);
+    // console.log("inside ready"+formData)
+    // addCards(cardList);
     $('.modal').modal();
+    getAllCards();
 });
-
--- server.js
+++ server.js
@@ -1,9 +1,128 @@
+// var express = require("express")
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// var app = express()
+// //const uri = `mongodb+srv://$ahmadusama200@gmail.com:$ahmadusama@$sit725.vnrvq9k.mongodb.net/?retryWrites=true&w=majority&appName=SIT725`;
+
+// const uri = 'mongodb+srv://usamaahmad1234:5nJHLDdF3BHmv5vv@sit725.vnrvq9k.mongodb.net/?retryWrites=true&w=majority';
+// var port = process.env.PORT || 3000;
+// let collection;
+// app.use(express.static(__dirname + '/public'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// const client = new MongoClient(uri, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     }
+// }
+
+// );
+// async function runDBconnection() {
+//     try {
+//         await client.connect();
+//         collection = client.db().collection('Cat');
+//         console.log(collection);
+//     }
+//     catch (ex) {
+//         console.error(ex);
+//     }
+// }
+// app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+// });
+// app.get('/', (req, res) => {
+//     res.sendFile('index.html');
+// });
+// app.get('/api/cards', (req, res) => {
+//     getallcards((err, result) => {
+//         if (!err) {
+//             res.json({ statusCode: 200, data: result, message: 'get all cards ' });
+//         }
+//     });
+// });
+
+// runDBconnection() 
+
 var express = require("express")
 var app = express()
-app.use(express.static(__dirname+'/public'))
+let port = process.env.port || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = 'mongodb+srv://usamaahmad1234:5nJHLDdF3BHmv5vv@sit725.vnrvq9k.mongodb.net/?retryWrites=true&w=majority';
+let collection;
+
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-var port = process.env.port || 3000;
-app.listen(port,()=>{
-console.log("App listening to: "+port)
-})
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function runDB() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        collection = client.db().collection('Cats');
+        console.log(collection);
+    } catch (ex) {
+        console.error(ex);
+    }
+}
+async function postCard(card) {
+    try {
+        const result = await collection.insertOne(card);
+        return result;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+
+async function getAllCards() {
+    try {
+        const cats = await collection.find({}).toArray();
+        return cats;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+
+app.get('/', function (req, res) {
+    res.render('index.html');
+});
+
+app.post('/api/card', async (req, res) => {
+    try {
+        const card = req.body;
+        const result = await postCard(card);
+        res.json({ statusCode: 201, data: result, message: 'success' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ statusCode: 500, message: 'Internal server error' });
+    }
+});
+
+
+
+app.get('/api/cards', async (req, res) => {
+    try {
+        const result = await getAllCards();
+        res.json({ statusCode: 200, data: result, message: 'get all cards successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ statusCode: 500, message: 'Internal server error' });
+    }
+});
+
+app.listen(port, () => {
+    console.log('express server started');
+    runDB();
+});
